1403ae85865223da6af84d7ef7ea359c79e1171e
Byanna13 <fabianasouza012@gmail.com>
2023-02-08 09:07:23 -0300

    2update 8fev

--- recipes/views.py
+++ recipes/views.py
@@ -6,20 +6,20 @@ from utils.recipes.factory import make_recipe
 # HTTP Request
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(9)], 'name': 'Byanna Burguer',
+        'recipes': [make_recipe() for _ in range(9)], 'name': 'Burguer',
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(), 'name': 'Byanna Burguer',
+        'recipe': make_recipe(), 'name': 'Burguer',
         'is_detail_page' : True,
     })
 
 def vai(request):
-    return render(request,'recipes/part/vai.html', context={'name': 'Byanna Burguer'})
+    return render(request,'recipes/part/vai.html', context={'name': 'Burguer'})
 
 def contato(request):
-    return render(request,'recipes/part/contato.html', context={'name': 'Byanna Burguer'})
+    return render(request,'recipes/part/contato.html', context={'name': 'Burguer'})
   
 # def contato(request):
 #     # HTTP Response
